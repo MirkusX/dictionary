19ff9fc46b6925e55f7c4b615e71e41c7f852347
MirkusX <marcusk.kodehode@gmail.com>
2023-02-09 10:19:15 +0100

    feat/preload audio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Frontpage } from "./Pages/Frontpage";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { DataContext } from "./Components/Context";
 
 function App() {
-  return (
-    <div className="App">
-      <Frontpage />
-    </div>
-  );
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const getData = () => {
+    axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/en/hello`)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((res) => {
+        setError(res);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  if (data) {
+    return (
+      <DataContext.Provider value={{ data, setData }}>
+        <div className="App">
+          <Frontpage />
+        </div>
+      </DataContext.Provider>
+    );
+  }
 }
 
 export default App;
--- src/Pages/Frontpage.js
+++ src/Pages/Frontpage.js
@@ -1,9 +1,32 @@
+import { useContext, useEffect, useState } from "react";
+import { DataContext } from "../Components/Context";
+
 export const Frontpage = () => {
+  const { data, setData } = useContext(DataContext);
+  const [audio, setAudio] = useState();
+  useEffect(() => {
+    data.map((item, index) => {
+      item.phonetics.slice(1, 2).map((item, index) => {
+        setAudio(new Audio(item.audio));
+        console.log(audio);
+      });
+    });
+  }, []);
+
   return (
     <section>
       <div>
         <h1>Frontpage</h1>
       </div>
+      {data.map((item, index) => {
+        return (
+          <div key={index}>
+            <div>
+              <h1>{item.word}</h1>
+            </div>
+          </div>
+        );
+      })}
     </section>
   );
 };
